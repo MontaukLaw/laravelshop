891d02b217de0ec058540ad2e29228b7c3405451
Marc <zunly@hotmail.com>
2019-03-09 14:30:56 +0800

    修改和删除收货地址

--- resources/js/app.js
+++ resources/js/app.js
@@ -32,6 +32,8 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 // 此处需在引入 Vue 之后引入
 require('./components/SelectDistrict');
 require('./components/UserAddressesCreateAndEdit');
+require('sweetalert');
+
 
 const app = new Vue({
     el: '#app'
