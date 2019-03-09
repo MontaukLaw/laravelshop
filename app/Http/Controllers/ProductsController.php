3446ccc7014f1a78980265fbbd2c3ab302594da6
Marc <zunly@hotmail.com>
2019-03-09 20:48:06 +0800

    用户界面, 商品详情页

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -7,6 +7,17 @@ use App\Models\Product;
 
 class ProductsController extends Controller
 {
+
+    public function show(Product $product, Request $request)
+    {
+        // 判断商品是否已经上架，如果没有上架则抛出异常。
+        if (!$product->on_sale) {
+            throw new \Exception('商品未上架');
+        }
+
+        return view('products.show', ['product' => $product]);
+    }
+
     public function index(Request $request)
     {
         // 创建一个查询构造器
