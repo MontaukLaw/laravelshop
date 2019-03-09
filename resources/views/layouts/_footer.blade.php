0e371590290137a135ff50c473384e203bba3c0b
Marc <zunly@hotmail.com>
2019-03-09 18:56:14 +0800

    用户界面, 商品列表

--- resources/views/layouts/_footer.blade.php
+++ resources/views/layouts/_footer.blade.php
@@ -1,9 +1,16 @@
 <footer class="footer">
-  <div class="container">
-    <p class="float-left">
-由 <a href="https://leo108.com" target="_blank">Leo</a> 设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
-    </p>
+    <div class="container">
+        <p class="float-left">
+            由 <a href="https://www.cnblogs.com/Montauk/" target="_blank">Marc</a>
+            设计和编码 <span style="color: #e27575;font-size: 14px;">❤</span>
+        </p>
 
-    <p class="float-right"><a href="mailto:name@email.com">联系我们</a></p>
-  </div>
+        <p class="float-right">
+            <a href="4606973@qq.com">
+                联系我
+            </a>
+            <i class="fa fa-users"></i>
+            4606973
+        </p>
+    </div>
 </footer>
